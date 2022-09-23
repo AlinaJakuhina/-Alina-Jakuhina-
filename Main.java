7978e342b0347b695f6b3ad5ea0d534886627cc1
AlinaJakuhina <113685502+AlinaJakuhina@users.noreply.github.com>
2022-09-23 23:11:23 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main
+{ public static void main(String[] args) {
+        char a = 'g';
+        System.out.println(a);
+        int b = 89;
+        System.out.println(b);
+        byte c = 4;
+        System.out.println(c);
+        short d = 56;
+        System.out.println(d);
+        float e = 4.7333436f;
+        System.out.println(e);
+        double f = 4.355453532;
+        System.out.println(f);
+        long h = 12121;
+        System.out.println(h);
+    }
+    }
+
+
+
